287fb7f29a544ed55d85e79255301f8ec61b71ba
Az21 <39803478+Az-21@users.noreply.github.com>
2021-04-19 19:44:12 +0530

    fix: created function for `roots` instead of using built-in

--- matlab/assignments/a1.m
+++ matlab/assignments/a1.m
@@ -1,30 +1,31 @@
 % Class #1 Assignment
 % Saturday | April 17, 2021
 
-clc; clear; close all;
-
 %% Problem 1
+clc; clear; close all;
 x = [1, 2, 3, 4];
-y = x * x'; % [x] * T([x])
+y = x * x; % [x] * T([x])
 disp(y);
 
 %% Problem 2
+clc; clear; close all;
 x = 1.000000001;
 fx = uint64(round((log10(x) + exp(-x)) / (log(x))));
 disp(fx);
 
 %% Problem 3
-polynomial = [5, 6, 7];
-root = roots(polynomial);
-disp(root);
+clc; clear; close all;
+p = [5, 6, 7];
+a = p(1); b = p(2); c = p(3);
+root1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+root2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+disp(root1);
+disp(root2);
 
 %% Problem 4
+clc; clear; close all;
 N = 4;
 omega = 2 * pi / N;
 n = 1:5;
-z = ones(1, length(n));
-
-for i = 1:length(n)
-    z(i) = exp(-1i * omega * i) * sin(omega * i) + 4 * cos(omega * i) / 5;
-    disp(z(i));
-end
+z = exp(-1i * omega .* n) .* sin(omega .* n) + 4 .* cos(omega .* n) / 5;
+disp(z);
