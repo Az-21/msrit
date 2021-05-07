51c222308c50a71c07e1ac90c99982234a295105
Az21 <39803478+Az-21@users.noreply.github.com>
2021-05-07 23:22:16 +0530

    added more steps

--- mosfet_simulation/DimensionGenerator/Program.cs
+++ mosfet_simulation/DimensionGenerator/Program.cs
@@ -6,16 +6,16 @@ namespace DimensionGenerator {
         static void Main(string[] args) {
             // Console colors
             string text = "#3AC769";
-            string value = "#FFFFFF";
+            string number = "#FFFFFF";
             string heading = "#31A8FF";
 
             // *----------------- PARAMS -----------------* //
             // *------------------------------------------* // 
-            Console.WriteLine("MOSFET Dimensions and Voltage".Pastel(heading));
-            Console.WriteLine("-----------------------------".Pastel(heading));
+            Console.WriteLine("MOSFET Dimensions".Pastel(heading));
+            Console.WriteLine("-----------------".Pastel(heading));
 
-            //Console.Write("→ Channel type   (p/n) = ".Pastel(text));
-            //char channelType = Console.ReadLine().ToLower()[0];
+            Console.Write("→ Channel type   (p/n) = ".Pastel(text));
+            char channelType = Console.ReadLine().ToLower()[0];
 
             Console.Write("→ Channel length  (nm) = ".Pastel(text));
             double channelWidth = Convert.ToDouble(Console.ReadLine());
@@ -25,7 +25,14 @@ namespace DimensionGenerator {
 
             // *-------------- CALCULATIONS --------------* //
             // *------------------------------------------* // 
-            double perunit = channelWidth / 160;
+
+            //double channelWidth = 160;
+            //double oxideThickness = 3;
+            //char channelType = 'n';
+
+
+            double perunit = channelWidth / 160; // scaling factor
+            double surfaceThickness = 3 * perunit; // ----------------------TODO: find if it scales or same as width
             double rectH = 70 * perunit;
             double rectW = 300 * perunit;
             double poly1 = 50 * perunit; // Source
@@ -35,21 +42,73 @@ namespace DimensionGenerator {
             double poly5 = 300 * perunit; // Drain
 
 
+            string dopeBase = "";
+            string dopeEdge = "";
+            if(channelType == 'n') {
+                dopeBase = "Acceptor doping (p-type)";
+                dopeEdge = "Donator doping (n-type)";
+            } else if(channelType == 'p') {
+                dopeBase = "Donator doping (p-type)";
+                dopeEdge = "Acceptor doping (n-type)";
+            } else {
+                Console.WriteLine($"\n\n\nInvalid channel type: '{channelType}'. Accepted responses are 'p' and 'n'");
+                Console.WriteLine("Restart the console app. Press any key to exit ...");
+                Console.ReadLine();
+                Environment.Exit(0);
+
+            }
+
+
             // *----------------- OUTPUT -----------------* //
             // *------------------------------------------* // 
             Console.WriteLine("\nSteps to produce your MOSFET".Pastel(heading));
             Console.WriteLine("----------------------------".Pastel(heading));
 
-            Console.WriteLine("→ Set length unit to nanometer (nm)".Pastel(text));
+            Console.WriteLine("→ Set length unit to nanometer (nm)\n".Pastel(text));
 
-            Console.WriteLine($"→ Rectangle dimensions:  {rectW.ToString().Pastel(value)} x {rectH.ToString().Pastel(value)}".Pastel(text));
+            // Rectangle
+            Console.WriteLine($"→ Rectangle dimensions: {rectW.ToString().Pastel(number)} x {rectH.ToString().Pastel(number)}\n".Pastel(text));
 
-            Console.Write($"→ Polygon coordinates x: ".Pastel(text));
-            Console.WriteLine($"0 0 {poly1} {poly2} {poly3} {poly4} {poly5} {poly5}".Pastel(value));
+            // Polygon
+            Console.WriteLine($"→ Polygon (vector) dimensions:".Pastel(text));
+            Console.Write($"    → x: ".Pastel(text));
+            Console.WriteLine($"0 0 {poly1} {poly2} {poly3} {poly4} {poly5} {poly5}".Pastel(number));
 
-            Console.Write($"→ Polygon coordinates y: ".Pastel(text));
-            Console.WriteLine($"{rectH - oxideThickness} {rectH} {rectH} {rectH} {rectH} {rectH} {rectH} {rectH - oxideThickness}".Pastel(value));
+            Console.Write($"    → y: ".Pastel(text));
+            Console.WriteLine($"{rectH - surfaceThickness} {rectH} {rectH} {rectH} {rectH} {rectH} {rectH} {rectH - surfaceThickness}\n".Pastel(number));
 
+            // Doping 1
+            Console.WriteLine($"→ Analytic Doping Model 1".Pastel(text));
+            Console.WriteLine($"    → {dopeBase.Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → {"1e17[1/cm^3]".Pastel(number)}\n".Pastel(text));
+
+            // Doping 2
+            Console.WriteLine($"→ Analytic Doping Model 2".Pastel(text));
+            Console.WriteLine($"    → {dopeEdge.Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → {"1e20[1/cm^3]".Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Base position (ro): {$"{0}, {60 * perunit}".Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Width (W): {(60 * perunit).ToString().Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Height (D): {(10 * perunit).ToString().Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Junction depth (dj): {$"{20 * perunit}, {25 * perunit}".Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Background doping concentration (Nb): {"1e17[1/cm^3]".Pastel(number)}\n".Pastel(text));
+
+            // Doping 3
+            Console.WriteLine($"→ Analytic Doping Model 3".Pastel(text));
+            Console.WriteLine($"    → Copy-paste Analytic Doping Model 3".Pastel(text));
+            Console.WriteLine($"    → Base position (ro): {$"{240 * perunit}, {60 * perunit}".Pastel(number)}\n".Pastel(text));
+
+            // Metal contacts
+            Console.WriteLine($"→ Metal Contact 1, 2, 3 → Follow video\n".Pastel(text));
+
+            // Thin insulator gate
+            Console.WriteLine($"→ Thin Insulator Gate 1".Pastel(text));
+            Console.WriteLine($"    → Oxide relative permability (Eins): {"4.5".Pastel(number)}".Pastel(text));
+            Console.WriteLine($"    → Oxide thickness (d_ins): {$"{oxideThickness}[nm]".Pastel(number)}\n".Pastel(text));
+
+            // Final
+            Console.WriteLine($"→ Follow video to complete design".Pastel(text));
+
+            Console.WriteLine("\n\n\nPress any key to exit ...");
             Console.ReadLine();
         }
     }
