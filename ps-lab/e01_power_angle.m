04c3337fa90c9959d3ad450520f0542ce859feaf
Az-21 <39803478+Az-21@users.noreply.github.com>
2021-11-11 19:28:36 +0530

    power angle characteristics 2 cases

---
+++ ps-lab/e01_power_angle.m
@@ -0,0 +1,70 @@
+% Experiment #1a
+% Power Angle Characteristics (Salient Pole)
+clc; clear; close all;
+
+% Parameters
+Ra = 0;
+Xd = 1;
+Xq = 0.6;
+V = 1;
+pf = 1;
+E = 1.1;
+delta = 0:1:180;
+
+% Power
+E = abs(E);
+V = abs(V);
+
+P_m1 = abs(E) * abs(V) * sin(delta .* pi/180) / Xd;
+P_m2 = V * V * (Xd - Xq) * sin(2 * delta .* pi/180) / (2 * Xd * Xq);
+
+% Plot
+plot(delta, P_m1);          % real
+hold on;
+plot(delta, P_m2);          % reluctance
+hold on;
+plot(delta, P_m1 + P_m2);   % total
+
+% Plot styling
+grid on;
+title('Power Angle Characteristics of Salient Pole Sync Machine');
+xlabel('\delta (degree)');
+ylabel('Power (p.u.)');
+legend('P_{m1}: Real Power', 'P_{m2}: Reluctance Power', 'P_T   : Total Power');
+
+%%
+
+% Experiment #1b
+% Power Angle Characteristics (Non-Salient Pole)
+clc; clear; close all;
+
+% Parameters
+Ra = 0;
+Xs = 0.3;
+V = 1;
+pf = 0.8;
+delta = 0:1:180;
+P = 0.5;
+
+% Calculating |E|
+I = P / (V * pf);
+
+phi = acos(pf);
+Ia = I * cos(phi) - 1j * I * sin(phi);
+
+Z = Ra + 1j * Xs;
+E = V + Ia * Z;
+
+% Power
+E = abs(E);
+V = abs(V);
+
+P = E * V * sin(delta .* pi/180) / Xs;
+
+% Plot
+plot(delta, P);
+% Plot styling
+grid on;
+title('Power Angle Characteristics of Non-Salient Pole Sync Machine');
+xlabel('\delta (degree)');
+ylabel('Power (p.u.)');
