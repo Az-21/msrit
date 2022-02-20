b4872d1a31f6f977b8e4a199cfcc0fbe125b13bf
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-20 09:57:22 +0530

    formatting fix + output in angle form

--- ps-lab/e02_transmission_line_params.m
+++ ps-lab/e02_transmission_line_params.m
@@ -1,15 +1,15 @@
-% Experiment 2
-% Transmission line parameters
-% Abhishek Choudhary
-% 1MS18EE003
 clc; clear; close all;
+
+% ---------------------------------------------------------------------------- %
+%                         Transmission line parameters                         %
+% ---------------------------------------------------------------------------- %
 
 % Parameters
 L = 130;
 Z1 = complex(0.036, 0.3);
-Y1 = complex(0, 4.22 * 10 ^ (- 6));
-Vr_ll = 325 * 10 ^ 3;
-Ps = 270 * 10 ^ 6;
+Y1 = complex(0, 4.22 * 10^(- 6));
+Vr_ll = 325 * 10^3;
+Ps = 270 * 10^6;
 pf = 0.8;
 config = 'T'; % T/P
 
@@ -23,7 +23,7 @@ if L <= 80
     B = Z;
     C = 0;
     D = 1;
- 
+
     % Medium transmission line (T-config)
 elseif L > 80 && L <= 240 && config == 'T'
     fprintf('Medium transmission line (T configuration)');
@@ -31,7 +31,7 @@ elseif L > 80 && L <= 240 && config == 'T'
     B = Z * (1 + (Y * Z) / 4);
     C = Y;
     D = A;
- 
+
     % Medium transmission line (Pi-config)
 elseif L > 80 && L <= 240 && config == 'P'
     fprintf('Medium transmission line (Pi configuration)');
@@ -39,7 +39,7 @@ elseif L > 80 && L <= 240 && config == 'P'
     B = Z;
     C = Y * (1 + (Y * Z) / 4);
     D = A;
- 
+
     % Long transmission line
 else
     fprintf('Long transmission line');
@@ -73,8 +73,8 @@ ADminusBC = A * D - B * C;
 % Output
 fprintf('\nL = %d km\n', L);
 fprintf('Z = %f + %fi ohm/km\n', real(Z1), imag(Z1));
-fprintf('Y = %fi umho/km\n', imag(Y1) * 10 ^ 6);
-fprintf('Pr = %d MVA\n', Ps / 10 ^ (6));
+fprintf('Y = %fi umho/km\n', imag(Y1) * 10^6);
+fprintf('Pr = %d MVA\n', Ps / 10^(6));
 fprintf('Vr_ll = %d kV\n', Vr_ll / 1000);
 fprintf('Power factor = %f\n\n', pf);
 fprintf('A = %f + %fi\n', real(A), imag(A));
@@ -82,7 +82,7 @@ fprintf('B = %f + %fi\n', real(B), imag(B));
 fprintf('C = %f + %fi\n', real(C), imag(C));
 fprintf('D = %f + %fi\n', real(D), imag(D));
 fprintf('AD - BC = %f + %fi\n\n', real(ADminusBC), imag(ADminusBC));
-fprintf('Vs = %f + %fi V\n', real(Vs_ph), imag(Vs_ph));
-fprintf('Is = %f + %fi A\n', real(Is_ph), imag(Is_ph));
+fprintf('Vs = %f ∠%f° kV\n', abs(Vs_ph) / 1000, angle(Vs_ph) * 180 / pi);
+fprintf('Is = %f ∠%f° kA\n', abs(Is_ph) / 1000, angle(Is_ph) * 180 / pi);
 fprintf('Voltage Regulation = %f %%\n', regulation);
 fprintf('Efficiency = %f %%', efficiency);
