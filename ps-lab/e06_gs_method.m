73ae42c021bdf9134250c92a77c340d63a7b9b4e
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-20 10:04:12 +0530

    fix formatting

--- ps-lab/e06_gs_method.m
+++ ps-lab/e06_gs_method.m
@@ -1,15 +1,15 @@
-% Experiment 6
-% GS Method
-% Abhishek Choudhary
-% 1MS18EE003
 clc; clear; close all;
+
+% ---------------------------------------------------------------------------- %
+%                              Gauss–Seidel Method                             %
+% ---------------------------------------------------------------------------- %
 
 n = 3;
 V = [1.05, 1, 1];
 P = [inf, -2.566, -1.386];
 Q = [inf, -1.102, -0.452];
 
-Y =[[complex(20, - 50), complex(- 10, 20), complex(- 10, 30)],
+Y = [[complex(20, - 50), complex(- 10, 20), complex(- 10, 30)],
     [complex(- 10, 20), complex(26, - 52), complex(- 16, 32)],
     [complex(- 10, 30), complex(- 16, 32), complex(26, - 62)]];
 
@@ -21,18 +21,23 @@ fprintf('Ybus = \n');
 disp(Y);
 
 % GS Loop
-delta = 1; % error placeholder
+delta = 1; % error between two consecutive iterations
 iterationCount = 1;
 
 while (delta > 0.00001)
     V_prev = V;
+
     for i = 2:n
         sumyv = 0;
+
         for k = 1:n
+
             if (i ~= k)
                 sumyv = sumyv + Y(i, k) * V(k);
             end
+
         end
+
         V(i) = (1 / Y(i, i)) * ((P(i) - 1j * Q(i)) / conj(V(i)) - sumyv);
     end
 
