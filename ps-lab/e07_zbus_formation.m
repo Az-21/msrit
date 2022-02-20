0bfb454bb47a4e91b60b54e88d3f96f92a72e36d
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-21 00:35:13 +0530

    code repetition fix and minor changes

--- ps-lab/e07_zbus_formation.m
+++ ps-lab/e07_zbus_formation.m
@@ -4,7 +4,7 @@ clc; clear; close all;
 %                                Z Bus Formation                               %
 % ---------------------------------------------------------------------------- %
 
-% Start, end, impedance, modification type (new/existing)
+% Start, end, impedance, modification type (1/2/3/4)
 data = [
     [1, 2, 0.1, 1]
     [2, 3, 0.5, 2]
@@ -17,43 +17,35 @@ stepCount = 0;
 
 % -------------------------- Zbus building algorithm ------------------------- %
 for i = 1:elements
+    % Initialize values
+    sb = data(i, 1);
+    eb = data(i, 2);
+    oncomingElement = data(i, 3);
+
+    % Swap values if starting bus > ending bus
+    if (sb > eb)
+        temp = sb;
+        sb = eb;
+        eb = temp;
+    end
 
     % Type 1 and Type 2 modification
     if (data(i, 4) <= 2)
-        sb = data(i, 1);
-        eb = data(i, 2);
-
-        if (sb > eb)
-            temp = sb;
-            sb = eb;
-            eb = temp;
-        end
-
         zncol = Zbus(:, sb);
         znrow = Zbus(sb, :);
-        zn_dia = Zbus(sb, sb) + data(i, 3);
+        zn_dia = Zbus(sb, sb) + oncomingElement;
         Zbus = [Zbus zncol; znrow zn_dia];
 
     % Type 3 and Type 4 modification
     else
-        sb = data(i, 1);
-        eb = data(i, 2);
-
-        if (sb > eb)
-            temp = sb;
-            sb = eb;
-            eb = temp;
-        end
-
         zncol = Zbus(:, sb) - Zbus(:, eb);
         znrow = Zbus(sb, :) - Zbus(eb, :);
-        zn_dia = Zbus(sb, sb) + Zbus(eb, eb) - 2 * Zbus(sb, eb) + data(i, 3);
-        Zbus = [Zbus] - [(zncol * znrow) / zn_dia];
+        zn_dia = Zbus(sb, sb) + Zbus(eb, eb) - 2 * Zbus(sb, eb) + oncomingElement;
+        Zbus = [Zbus] - [(zncol * znrow) / zn_dia]; % size of matrix remains same
     end
 
-    stepCount = stepCount + 1;
-
     % Output
+    stepCount = stepCount + 1;
     fprintf('Step %d | Type %d modification\n', stepCount, data(i, 4));
 
     if i ~= elements
