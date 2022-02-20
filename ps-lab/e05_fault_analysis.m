df7804479bbc5c7ef1d1f8c0a0f57d865c1e5b52
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-20 10:02:27 +0530

    formatting fix

--- ps-lab/e05_fault_analysis.m
+++ ps-lab/e05_fault_analysis.m
@@ -1,12 +1,12 @@
-% Experiment 5
-% Fault analysis
-% Abhishek Choudhary
-% 1MS18EE003
 clc; clear; close all;
 
+% ---------------------------------------------------------------------------- %
+%                                Fault analysis                                %
+% ---------------------------------------------------------------------------- %
+
 % Params
-P = 20 * 10 ^ 6;
-I = 13.8 * 10 ^ 3;
+P = 20 * 10^6;
+I = 13.8 * 10^3;
 Z1 = complex(0, 0.25);
 Z2 = complex(0, 0.35);
 Z0 = complex(0, 0.10);
@@ -21,54 +21,53 @@ fprintf('3 -> Double line to ground fault');
 faultType = input('');
 fprintf('\n');
 
-% // Line to ground fault
+% --------------------------- Symmertic components --------------------------- %
+
+% Line to ground fault
 if faultType == 1
     fprintf('Single line to ground fault selected\n');
- 
-    % Symmetric components
+
     Ia1_pu = Ea / (Z0 + Z1 + Z2);
     Ia2_pu = Ia1_pu;
     Ia0_pu = Ia1_pu;
 
     Va1_pu = Ea - Ia1_pu * Z1;
-    Va2_pu = - 1 * Ia2_pu * Z2;
-    Va0_pu = - 1 * Ia0_pu * Z0;
- 
-% // Line to line fault
+    Va2_pu =- 1 * Ia2_pu * Z2;
+    Va0_pu =- 1 * Ia0_pu * Z0;
+
+% Line to line fault
 elseif faultType == 2
     fprintf('Line to line fault selected\n');
 
-    % Symmertic components
     Ia1_pu = Ea / (Z1 + Z2);
-    Ia2_pu = - 1 * Ia1_pu;
+    Ia2_pu =- 1 * Ia1_pu;
     Ia0_pu = 0;
-    
+
     Va1_pu = Ea - Ia1_pu * Z1;
     Va2_pu = Va1_pu;
     Va0_pu = 0; % neutral is grounded
-    
-% // Double line to ground fault
+
+% Double line to ground fault
 else
     fprintf('Double line to ground fault selected\n');
- 
-    % Symmetric components
+
     Ia1_pu = Ea / (Z1 + Z2 * Z0 / (Z2 + Z0));
     Va1_pu = Ea - Ia1_pu * Z1;
     Va2_pu = Va1_pu;
     Va0_pu = Va1_pu;
-    Ia2_pu = - 1 * Va2_pu / Z2;
-    Ia0_pu = - 1 * Va0_pu / Z0;
+    Ia2_pu =- 1 * Va2_pu / Z2;
+    Ia0_pu =- 1 * Va0_pu / Z0;
 end
 
 % Fault currents
 Ia_pu = Ia1_pu + Ia2_pu + Ia0_pu;
-Ib_pu = alpha ^ 2 * Ia1_pu + alpha * Ia2_pu + Ia0_pu;
-Ic_pu = alpha * Ia1_pu + alpha ^ 2 * Ia2_pu + Ia0_pu;
+Ib_pu = alpha^2 * Ia1_pu + alpha * Ia2_pu + Ia0_pu;
+Ic_pu = alpha * Ia1_pu + alpha^2 * Ia2_pu + Ia0_pu;
 
 % Fault voltages
 Va_pu = Va1_pu + Va2_pu + Va0_pu;
-Vb_pu = alpha ^ 2 * Va1_pu + alpha * Va2_pu + Va0_pu;
-Vc_pu = alpha * Va1_pu + alpha ^ 2 * Va2_pu + Va0_pu;
+Vb_pu = alpha^2 * Va1_pu + alpha * Va2_pu + Va0_pu;
+Vc_pu = alpha * Va1_pu + alpha^2 * Va2_pu + Va0_pu;
 
 % Line to line voltages
 Vab_pu = Va_pu - Vb_pu;
@@ -88,8 +87,8 @@ Vbc = Vbc_pu * base;
 Vca = Vca_pu * base;
 
 % Output (in angle form)
-fprintf('P = %f MVA\n', P * 10 ^ (- 6));
-fprintf('I = %f kA\n', I * 10 ^ (- 3));
+fprintf('P = %f MVA\n', P * 10^(- 6));
+fprintf('I = %f kA\n', I * 10^(- 3));
 fprintf('Z1 = %fj p.u.\n', imag(Z1));
 fprintf('Z2 = %fj p.u.\n', imag(Z2));
 fprintf('Z0 = %fj p.u.\n\n', imag(Z0));
