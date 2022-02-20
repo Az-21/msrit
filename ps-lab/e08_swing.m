4bd5ce0d6e02d714a94aeb8e72385ef95c1857a0
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-21 01:09:28 +0530

    formatting fix

--- ps-lab/e08_swing.m
+++ ps-lab/e08_swing.m
@@ -5,25 +5,24 @@ clc; clear; close all;
 % ---------------------------------------------------------------------------- %
 
 % Params
+f = 50; % frequency
+M = 0.016; % inertia
+V = 1; % terminal voltage
+Eg = 1.1; % generator voltage
+Pe = 0.9; % ¯\_(ツ)_/¯
 x1 = 0.45; % transfer reactance before fault
 x2 = 1.25; % transfer reactance during fault
 x3 = 0.55; % transfer reactance after fault
-Eg = 1.1; % generator voltage
-V = 1; % terminal voltage
-Pe = 0.9; %
-f = 50; % frequency
-M = 0.016; % inertia
 
 % Calculations
 Pm = Pe;
-Pm1 = Eg*V/x1;
-Pm2 = Eg*V/x2;
-Pm3 = Eg*V/x3;
+Pm1 = Eg * V / x1;
+Pm2 = Eg * V / x2;
+Pm3 = Eg * V / x3;
 
-d0 = asin(Pm/Pm1);
-dmax = pi - asin(Pm/Pm3);
+d0 = asin(Pm / Pm1);
+dmax = pi - asin(Pm / Pm3);
 
-dr = ((Pm*(dmax-d0)) - (Pm2*cos(d0)) + (Pm3*cos(dmax)))/(Pm3 - Pm2);
-dcr1 = acos(dr);
-dcr = acosd(dr);
-tcr = sqrt((2*M*(dcr1-d0)/(Pm)));
+dr = ((Pm * (dmax - d0)) - (Pm2 * cos(d0)) + (Pm3 * cos(dmax))) / (Pm3 - Pm2);
+dcr = acos(dr);
+tcr = sqrt((2 * M * (dcr - d0) / (Pm)));
